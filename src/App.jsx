df5942970445aed082156eca263109ea3c2e3d4f
Kows3002 <kowsalyab313@gmail.com>
2024-12-21 11:19:15 +0530

    card created

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,44 @@ import './App.css';
 
 function Image() {
   return(
-    <div>
-    <h1 className='head'>PREMIUM DEVELOPERS</h1>
-    <div className='Container'>
-    <div>
+    <div className='head'>
+    <h1>Our Services</h1>
+    <div className='full-screen'>
+    <div className='card-container'>
     <img className='image'src='https://img.freepik.com/free-vector/programmer-working-with-php_52683-22998.jpg?t=st=1734408243~exp=1734411843~hmac=d7ee3555569e88b1adbb7d60de6cd87f2b0a7d069649ba229abb9f555ed2c5ff&w=740'/>
-    <p className='desc' style={{fontSize:"20PX", fontWeight:"bold"}}>Kiruthika</p>
-    <p className='desc'>FullStack Developer</p>
+    <p className='desc' style={{fontSize:'10px'}}>xxxxxhusjckskhc shcshckncekjn shchcgehfwjnljnd scguydgwbdkwnl</p>
+    <button className='button'>Read More</button>
     </div>
-    <div>
+    <div className='card-container'>
     <img className='image'src='https://img.freepik.com/free-vector/programmers-concept-with-flat-design_23-2147850105.jpg?t=st=1734409078~exp=1734412678~hmac=6eb9293c28076103a376e3955889bfc62fe14ff20303a5ee3a57a9d2e64a0c6c&w=740'/>
-    <p className='desc' style={{fontSize:"20PX", fontWeight:"bold"}}>Jeevenkumar</p>
-    <p className='desc'>FullStack Developer</p>
+    <p className='desc' style={{fontSize:"10PX"}}>xxxxxhusjckskhc shcshckncekjn shchcgehfwjnljnd scguydgwbdkwnl</p>
+    <button className='button'>Read More</button>
     </div>
-    <div>
+    <div className='card-container'>
     <img className='image'src='https://img.freepik.com/free-vector/programmer-working-desk_52683-24830.jpg?t=st=1734409652~exp=1734413252~hmac=f4633fd42dfbf463b2fb44242155e143d83cd553a4860e85d85d51a453c9eb2e&w=740'/>
-    <p className='desc' style={{fontSize:"20PX", fontWeight:"bold"}}>Kaniksha</p>
-    <p className='desc'>FullStack Developer</p>
+    <p className='desc' style={{fontSize:"10PX"}}>xxxxxhusjckskhc shcshckncekjn shchcgehfwjnljnd scguydgwbdkwnl</p>
+    <button className='button'>Read More</button>
+   
     </div>
-    <div>
+    <div className='card-container'>
     <img className='image'src='https://img.freepik.com/free-vector/cartoon-style-programmer-working_52683-22693.jpg?t=st=1734409703~exp=1734413303~hmac=1f4c329255dc5974cdde3c44a9da9c2a883fd6a0582373d7710d635b19509ed7&w=740'/>
-    <p className='desc' style={{fontSize:"20PX", fontWeight:"bold"}}>Kowsalya</p>
-    <p className='desc'>FrontEnd Developer</p>
+    <p className='desc' style={{fontSize:"10PX"}}>xxxxxhusjckskhc shcshckncekjn shchcgehfwjnljnd scguydgwbdkwnl</p>
+    <button className='button'>Read More</button>
+
     </div>
-    <div>
+    <div className='card-container'>
     <img className='image'src='https://img.freepik.com/free-vector/programmer-working-flat-style_52683-16623.jpg?t=st=1734409740~exp=1734413340~hmac=c47e23669b32a43c3ccc45d97bf8c919198b58ec686e5197dd9ef19afaa9e2ae&w=740'/>
-    <p className='desc' style={{fontSize:"20PX", fontWeight:"bold"}}>Navaneetha</p>
-    <p className='desc'>FullStack Developer</p>
+    <p className='desc' style={{fontSize:"10PX"}}>xxxxxhusjckskhc shcshckncekjn shchcgehfwjnljnd scguydgwbdkwnl</p>
+    <button className='button'>Read More</button>
+
     </div>
-    <div>
+    <div className='card-container'>
     <img className='image' src='https://img.freepik.com/free-vector/programmer-doing-his-job-flat-design_52683-14150.jpg?t=st=1734409777~exp=1734413377~hmac=682928d8bbd0d19c6493edc99877db1e5c3718e57c4c73e4eb43047bade10985&w=740'/>
-    <p className='desc' style={{fontSize:"20PX", fontWeight:"bold"}}>Darthy</p>
-    <p className='desc'>FullStack Developer</p>
+    <p className='desc' style={{fontSize:"10PX"}}>xxxxxhusjckskhc shcshckncekjn shchcgehfwjnljnd scguydgwbdkwnl</p>
+    <button className='button'>Read More</button>
+   
     </div>
-   </div>
+</div>
 </div>
   );
 }
